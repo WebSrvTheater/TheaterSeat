269451fbdda850f02b4bf6a2028a1d47e6b51dae
DESKTOP-340F6FN\ITS_1 <dh179@seoultech.ac.kr>
2018-11-06 18:45:47 +0900

    no message

---
+++ src/main/java/com/websrv/theaterseat/TheaterseatApplication.java
@@ -0,0 +1,12 @@
+package com.websrv.theaterseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterseatApplication.class, args);
+	}
+}
